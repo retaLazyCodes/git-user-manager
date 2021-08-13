513aaf621fc8c3b964363d1a663f9ee593ee1f83
Brian Retamar <leo1632@hotmail.com>
2021-08-12 22:04:34 -0300

    Added multi-platform support

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ require('electron-reload')(__dirname);
 
 app.allowRendererProcessReuse = true;
 app.on('ready', createWindow);
+
+app.on(
+    "window-all-closed",
+    () => process.platform !== "darwin" && app.quit()
+);
